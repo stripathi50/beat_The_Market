5c7efb8780fd2b1c811476d480b6b6231f93ced0
Sagar Tripathi <90290487+stripathi50@users.noreply.github.com>
2023-03-20 15:02:45 -0400

    Update Data_Preprocessing.py
    added models

--- Data PreProcessing/Data_Preprocessing.py
+++ Data PreProcessing/Data_Preprocessing.py
@@ -1,5 +1,23 @@
+# Imports and Helper Functions
+# data Analysis
 import pandas as pd
 import numpy as np
+import random as rng
+
+# Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#SciKit Learn Models
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 df_mma1=pd.read_csv('/Users/sagartripathi/Documents/pythonProject/MMA_Betting/data/masterdataframe.csv')
 print(df_mma1.head(5))
@@ -67,10 +85,10 @@ from datetime import datetime
 
 # convert date columns to datetime format
 # df_mma1['date'] = pd.to_datetime(df_mma1['date'])
-# df_mma1['date_of_birth'] = pd.to_datetime(df_mma1['date_of_birth'])
+# df_mma1['date_of_birth'] = pd.to_datetime(df_mma1['dob'])
 #
 # # calculate age
-# df_mma1['age'] = (df_mma1['date'] - df_mma1['date_of_birth']).astype('timedelta64[Y]')
+# df_mma1['age'] = (df_mma1['date'] - df_mma1['dob']).astype('timedelta64[Y]')
 #
 # # replace null values
 # most_common_age = df_mma1['age'].mode()[0]
@@ -134,7 +152,6 @@ pd.set_option('display.max_columns', None)
 name_counts = df_mma1['fighter'].value_counts()
 names_to_find = name_counts[name_counts > 1].index.tolist()
 
-#
 # #first we found the fighters names with different divisions they are fighting
 matching_records = df_mma1[df_mma1['fighter'].isin(names_to_find)]
 # print("*"*50)
@@ -160,15 +177,122 @@ for fighter in multi_div_fighters:
                          'division'] = common_division
 
 print(matching_records[['fighter', 'division', 'gender']].sort_values(by='fighter', ascending=True))
-
-print(df_mma1.head())
-
+# df_mma1['division']=df_mma1['fighter'].map(matching_records['division'])
 
 
-print(df_mma1.isnull().sum().sort_values())
+#
 
+df_mma1.to_csv('updated_fighters.csv', index=False)
 
 ############ EDA ##################################
+
+#data modelling
+
+ #We Store prediction of each model in our dict
+# Helper Functions for our models.
+
+def percep(X_train,Y_train,X_test,Y_test,Models):
+    perceptron = Perceptron(max_iter = 1000, tol = 0.001)
+    perceptron.fit(X_train, Y_train)
+    Y_pred = perceptron.predict(X_test)
+    Models['Perceptron'] = [accuracy_score(Y_test,Y_pred),confusion_matrix(Y_test,Y_pred)]
+    return
+
+def ranfor(X_train,Y_train,X_test,Y_test,Models):
+    randomfor = RandomForestClassifier(max_features="sqrt",
+                                       n_estimators = 700,
+                                       max_depth = None,
+                                       n_jobs=-1
+                                      )
+    randomfor.fit(X_train,Y_train)
+    Y_pred = randomfor.predict(X_test)
+    Models['Random Forests'] = [accuracy_score(Y_test,Y_pred),confusion_matrix(Y_test,Y_pred)]
+    return
+
+def dec_tree(X_train,Y_train,X_test,Y_test,Models):
+    decision_tree = DecisionTreeClassifier()
+    decision_tree.fit(X_train, Y_train)
+    Y_pred = decision_tree.predict(X_test)
+    Models['Decision Tree'] = [accuracy_score(Y_test,Y_pred),confusion_matrix(Y_test,Y_pred)]
+    return
+
+def SGDClass(X_train,Y_train,X_test,Y_test,Models):
+    sgd = SGDClassifier(max_iter = 1000, tol = 0.001)
+    sgd.fit(X_train, Y_train)
+    Y_pred = sgd.predict(X_test)
+    Models['SGD Classifier'] = [accuracy_score(Y_test,Y_pred),confusion_matrix(Y_test,Y_pred)]
+    return
+
+def linSVC(X_train,Y_train,X_test,Y_test,Models):
+    linear_svc = LinearSVC()
+    linear_svc.fit(X_train, Y_train)
+    Y_pred = linear_svc.predict(X_test)
+    Models['SVM'] = [accuracy_score(Y_test,Y_pred),confusion_matrix(Y_test,Y_pred)]
+    return
+
+def bayes(X_train,Y_train,X_test,Y_test,Models):
+    gaussian = GaussianNB()
+    gaussian.fit(X_train, Y_train)
+    Y_pred = gaussian.predict(X_test)
+    Models['Bayes'] = [accuracy_score(Y_test,Y_pred),confusion_matrix(Y_test,Y_pred)]
+    return
+
+def Nearest(X_train,Y_train,X_test,Y_test,Models):
+    knn = KNeighborsClassifier(n_neighbors = 3)
+    knn.fit(X_train, Y_train)
+    Y_pred = knn.predict(X_test)
+    Models['KNN'] = [accuracy_score(Y_test,Y_pred),confusion_matrix(Y_test,Y_pred)]
+
+def run_all_and_Plot(df):
+    Models = dict()
+    from sklearn.model_selection import train_test_split
+    X_all = df.drop(['winner'], axis=1)
+    y_all = df['winner']
+    X_train, X_test, Y_train, Y_test = train_test_split(X_all, y_all, test_size=0.2, random_state=0)
+    percep(X_train,Y_train,X_test,Y_test,Models)
+    ranfor(X_train,Y_train,X_test,Y_test,Models)
+    dec_tree(X_train,Y_train,X_test,Y_test,Models)
+    SGDClass(X_train,Y_train,X_test,Y_test,Models)
+    linSVC(X_train,Y_train,X_test,Y_test,Models)
+    bayes(X_train,Y_train,X_test,Y_test,Models)
+    Nearest(X_train,Y_train,X_test,Y_test,Models)
+    return Models
+
+
+def plot_bar(dict):
+    labels = tuple(dict.keys())
+    y_pos = np.arange(len(labels))
+    values = [dict[n][0] for n in dict]
+    plt.bar(y_pos, values, align='center', alpha=0.5)
+    plt.xticks(y_pos, labels,rotation='vertical')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy of different models')
+    plt.show()
+
+
+def plot_cm(dict):
+    count = 1
+    fig = plt.figure(figsize=(10,10))
+    for model in dict:
+        cm = dict[model][1]
+        labels = ['W','L','N','D']
+        ax = fig.add_subplot(4,4,count)
+        cax = ax.matshow(cm)
+        plt.title(model,y=-0.8)
+        fig.colorbar(cax)
+        ax.set_xticklabels([''] + labels)
+        ax.set_yticklabels([''] + labels)
+        plt.xlabel('Predicted')
+        plt.ylabel('True')
+        # plt.subplot(2,2,count)
+        count+=1
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+
 
 
 
