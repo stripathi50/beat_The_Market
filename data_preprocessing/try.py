ca53dc7e69dcdb8d18f8ce09ed07f2112ba0b66d
Sagar Tripathi <90290487+stripathi50@users.noreply.github.com>
2023-05-08 09:19:07 -0400

    not required

--- data_preprocessing/try.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-# Read the dataset into a pandas DataFrame
-data = pd.read_csv('updated_fighters3.csv')  # Replace 'fighters.csv' with the actual filename
-
